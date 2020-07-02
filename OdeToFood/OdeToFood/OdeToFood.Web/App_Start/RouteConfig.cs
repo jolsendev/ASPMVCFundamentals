8597a8708ac622a666be86958e4769347691838a
Jamie Olsen <a5w5nzz@mmm.com>
2020-07-02 17:20:10 -0600

    stuff

--- OdeToFood/OdeToFood/OdeToFood.Web/App_Start/RouteConfig.cs
+++ OdeToFood/OdeToFood/OdeToFood.Web/App_Start/RouteConfig.cs
@@ -15,6 +15,7 @@ namespace OdeToFood.Web
 
             routes.MapRoute(
                 name: "Default",
+
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
