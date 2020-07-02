60c8c120e08a4faa139d71d736848d984a6d9b8e
Jamie Olsen <jolsendev@gmail.com>
2020-07-02 12:42:26 -0700

    stuff

---
+++ OdeToFood/OdeToFood/OdeToFood.Data/Models/Resturant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace OdeToFood.Data.Models
+{
+    public class Resturant
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public CuisineType Cuisine { get; set; }
+
+    }
+}
---
+++ OdeToFood/OdeToFood/OdeToFood.Data/Services/InMemeoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemeoryRestaurantData : IRestaurantData
+    {
+        List<Resturant> resturants;
+        public InMemeoryRestaurantData()
+        {
+            resturants = new List<Resturant>()
+            {
+                new Resturant  { Id = 1, name = "Jamie Restaurant", Cuisine = CuisineType.French },
+                new Resturant  { Id = 2, name = "Data Restaurant",  Cuisine = CuisineType.Indian },
+                new Resturant  { Id = 3, name = "Dave Restaurant", Cuisine =  CuisineType.Indian },
+                new Resturant  { Id = 4, name = "Jon Restaurant",  Cuisine = CuisineType.None }
+            };
+        }
+        public IEnumerable<Resturant> GetAll()
+        {
+            return resturants.OrderBy(r => r.name);
+        }
+    }
+}
